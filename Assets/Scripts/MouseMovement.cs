bcdeb3a73862b547448fd4ddda167c8bf1f84bfa
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2023-10-24 13:28:53 +0100

    Add item glowing and fix some issues

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 250f;
 
     float xRotation = 0f;
     float YRotation = 0f;
