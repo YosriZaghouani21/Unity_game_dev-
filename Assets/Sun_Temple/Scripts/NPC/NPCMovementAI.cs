46abb96c4e50da330109cd771f538b4d260609e4
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2023-11-07 15:55:16 +0100

    baha

--- Assets/Sun_Temple/Scripts/NPC/NPCMovementAI.cs
+++ Assets/Sun_Temple/Scripts/NPC/NPCMovementAI.cs
@@ -9,19 +9,17 @@ public class NPCMovementAI : MonoBehaviour
     public GameObject theDestination;
     private NavMeshAgent theAgent;
     private bool isMoving = false;
-
     void Start()
     {
         animator = GetComponent<Animator>();
         theAgent = GetComponent<NavMeshAgent>();
+        theAgent.stoppingDistance = 1.0f;
     }
-
     public void MoveToLevel1()
     {
         animator.SetBool("IsWalking", true);
         theAgent.SetDestination(theDestination.transform.position);
         isMoving = true;
-
         if (isMoving && !theAgent.pathPending)
         {
             if (theAgent.remainingDistance <= theAgent.stoppingDistance)
@@ -30,16 +28,25 @@ public class NPCMovementAI : MonoBehaviour
                 {
                     // Arrived at the destination
                     animator.SetBool("IsWalking", false);
+                    print("AAAAAAAA");
                     animator.SetBool("ReachedDestination", true); // Transition to waving animation
                     isMoving = false;
                 }
             }
         }
+        StartCoroutine(CheckArrival());
     }
-
     void Update()
     {
 
-
+    }
+    private IEnumerator CheckArrival()
+    {
+        yield return new WaitUntil(() => !theAgent.pathPending);
+        while (Vector3.Distance(theAgent.transform.position, theDestination.transform.position) > theAgent.stoppingDistance)
+        {
+            yield return null;
+        }
+        animator.SetBool("IsWalking", false);
     }
 }
