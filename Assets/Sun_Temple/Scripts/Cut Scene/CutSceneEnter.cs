d965ab9de6687fe58769a6a36d987ed31369b954
bahaeddinelakhzouri <62963627+bahalakhzouri@users.noreply.github.com>
2023-11-13 18:12:47 +0000

    Added a temple cutscene HEEEE

---
+++ Assets/Sun_Temple/Scripts/Cut Scene/CutSceneEnter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneEnter : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public GameObject cutsceneCam;
+
+    void OnTriggerEnter(Collider other)
+    {
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        cutsceneCam.SetActive(true);
+        thePlayer.SetActive(false);
+        StartCoroutine(FinishCut());
+    }
+    IEnumerator FinishCut()
+    {
+        yield return new WaitForSeconds(5);
+        thePlayer.SetActive(true);
+        cutsceneCam.SetActive(false);
+    }
+}
