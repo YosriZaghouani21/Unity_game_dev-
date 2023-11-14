67e7e16c20cb55940d649aa4c73f5eadffae676b
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2023-11-14 15:29:12 +0100

    update npc movement lvl2

--- Assets/Sun_Temple/Scripts/NPC/NPCMovementAI.cs
+++ Assets/Sun_Temple/Scripts/NPC/NPCMovementAI.cs
@@ -1,52 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine;
+using System.Collections;
 
 public class NPCMovementAI : MonoBehaviour
 {
     private Animator animator;
     public GameObject theDestination;
+    public GameObject newDestination; 
     private NavMeshAgent theAgent;
     private bool isMoving = false;
+
     void Start()
     {
         animator = GetComponent<Animator>();
         theAgent = GetComponent<NavMeshAgent>();
         theAgent.stoppingDistance = 1.0f;
     }
+
     public void MoveToLevel1()
     {
+        Debug.Log("Moving to Level 1");
         animator.SetBool("IsWalking", true);
         theAgent.SetDestination(theDestination.transform.position);
         isMoving = true;
-        if (isMoving && !theAgent.pathPending)
-        {
-            if (theAgent.remainingDistance <= theAgent.stoppingDistance)
-            {
-                if (!theAgent.hasPath || theAgent.velocity.sqrMagnitude == 0f)
-                {
-                    // Arrived at the destination
-                    animator.SetBool("IsWalking", false);
-                    print("AAAAAAAA");
-                    animator.SetBool("ReachedDestination", true); // Transition to waving animation
-                    isMoving = false;
-                }
-            }
-        }
         StartCoroutine(CheckArrival());
     }
+
+    public void MoveToNewDestination()
+    {
+        Debug.Log("Moving to New Destination");
+        animator.SetBool("IsWalking", true);
+
+        Vector3 newDestination = new Vector3(-7.054f, 17.15f, -150.399f);
+        theAgent.SetDestination(newDestination);
+        isMoving = true;
+        StartCoroutine(CheckArrival());
+    }
+
     void Update()
     {
-
+        // Other update logic...
     }
+
     private IEnumerator CheckArrival()
     {
+        Debug.Log("Checking Arrival");
         yield return new WaitUntil(() => !theAgent.pathPending);
         while (Vector3.Distance(theAgent.transform.position, theDestination.transform.position) > theAgent.stoppingDistance)
         {
             yield return null;
         }
+        Debug.Log("Arrived at Destination");
         animator.SetBool("IsWalking", false);
+        isMoving = false;
     }
 }
