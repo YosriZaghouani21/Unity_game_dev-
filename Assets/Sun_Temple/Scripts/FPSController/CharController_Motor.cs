543efc38350fa3c670a473aa66ccab2d570fe306
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2023-11-26 21:00:36 +0100

    Update CharController_Motor.cs

--- Assets/Sun_Temple/Scripts/FPSController/CharController_Motor.cs
+++ Assets/Sun_Temple/Scripts/FPSController/CharController_Motor.cs
@@ -6,35 +6,88 @@ namespace SunTemple
 {
     public class CharController_Motor : MonoBehaviour
     {
+
+        public float speed = 10.0f;
+        public float sensitivity = 60.0f;
         CharacterController character;
+        public GameObject cam;
+        float moveFB, moveLR;
+        float rotHorizontal, rotVertical;
+        public bool webGLRightClickRotation = true;
         float gravity = -9.8f;
+
+        //string debugText;
+
 
         void Start()
         {
+
             character = GetComponent<CharacterController>();
-            SpawnOnGround();
+
+            webGLRightClickRotation = false;
+
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+            {
+                webGLRightClickRotation = true;
+                sensitivity = sensitivity * 1.5f;
+            }
+
+
         }
 
         void FixedUpdate()
         {
-            ApplyGravity();
-        }
+            moveFB = Input.GetAxis("Horizontal") * speed;
+            moveLR = Input.GetAxis("Vertical") * speed;
 
-        void ApplyGravity()
-        {
-            Vector3 gravityVector = new Vector3(0, gravity, 0);
-            character.Move(gravityVector * Time.fixedDeltaTime);
-        }
+            rotHorizontal = Input.GetAxisRaw("Mouse X") * sensitivity;
+            rotVertical = Input.GetAxisRaw("Mouse Y") * sensitivity;
 
-        void SpawnOnGround()
-        {
-            // Set the player's position to be on the ground
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, Vector3.down, out hit))
+
+            Vector3 movement = new Vector3(moveFB, gravity, moveLR);
+
+
+            if (webGLRightClickRotation)
             {
-                Vector3 spawnPosition = hit.point;
-                character.Move(spawnPosition - transform.position);
+                if (Input.GetKey(KeyCode.Mouse0))
+                {
+                    CameraRotation(cam, rotHorizontal, rotVertical);
+                }
             }
+            else if (!webGLRightClickRotation)
+            {
+                CameraRotation(cam, rotHorizontal, rotVertical);
+            }
+
+            movement = transform.rotation * movement;
+            character.Move(movement * Time.fixedDeltaTime);
         }
+
+        void CameraRotation(GameObject cam, float rotHorizontal, float rotVertical)
+        {
+
+            transform.Rotate(0, rotHorizontal * Time.fixedDeltaTime, 0);
+            cam.transform.Rotate(-rotVertical * Time.fixedDeltaTime, 0, 0);
+
+
+
+            if (Mathf.Abs(cam.transform.localRotation.x) > 0.7)
+            {
+
+                float clamped = 0.7f * Mathf.Sign(cam.transform.localRotation.x);
+
+                Quaternion adjustedRotation = new Quaternion(clamped, cam.transform.localRotation.y, cam.transform.localRotation.z, cam.transform.localRotation.w);
+                cam.transform.localRotation = adjustedRotation;
+            }
+
+
+        }
+
+
+
+
     }
+
+
+
 }
